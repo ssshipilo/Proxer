e91dcb43412eed52a0827952bd7a56b007bcf18d
ssshipilo <ssshipilo@gmail.com>
2024-07-09 02:53:22 +0200

    update code base and add new provider

--- __init__.py
+++ __init__.py
@@ -10,8 +10,10 @@ from requests import get
 from bs4 import BeautifulSoup
 from os import system
 from sys import platform
+import json
+import concurrent.futures
+import time
 
-#! The author of this code section: https://github.com/rodukov/proxyGrab/tree/main
 cls = lambda: system("cls") if platform in ["win32", "cygwin"] else system("clear")
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} # Be sure to add a browser header to bypass protection
 _blacklisted = ['IP address', 'Port', 'Country, City', '<td class="speed_col">Speed</td>', 'Type', 'Anonymity', 'Latest update']
@@ -20,31 +22,22 @@ _anonymity_type = ["High", "Average", "Low", "no"]
 _protocol_addons = {"HTTP": "h", "HTTPS": "s", "SOCKS4": "4", "SOCKS5": "5"} 
 _anonymity_addons = {"High": "4", "Average": "3", "Low": "2", "no": "1"}
 
-class proxyGrab:
+class proxy_scraper:
+
     class proxyscrape:
-        def request(protocol="http"):
-            url = f"https://api.proxyscrape.com/v2/?request=getproxies&protocol={protocol}&timeout=10000&country=all&ssl=all&anonymity=all"
+        def request():
+            url = f"https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=protocolipport&format=text&timeout=2000"
             request = get(url, headers=headers)
             return request 
         def sort(request) -> dict:
             api_data = request.text
             result = []
             for i in api_data.split("\r\n"):
-                try: result.append({"address": i.split(":")[0], "port": i.split(":")[1]})
-                except IndexError:...
+                if str(i.split(":")).strip() != "":
+                    try: result.append({"address": str(i.split(":")[1]).replace("//", ""), "port": i.split(":")[2], "protocol": i.split(":")[0]})
+                    except IndexError:...
             return result
-    class geonode:
-        def request(proxies=50):
-            url = f"https://proxylist.geonode.com/api/proxy-list?limit={proxies}&page=1&sort_by=lastChecked&sort_type=desc" # Using geonode API
-            request = get(url, headers=headers)
-            return request.text
-        def sort(request) -> dict:
-            print(request)
-            api_data = loads(request)
-            result = []
-            for i in api_data["data"]:
-                result.append({"address": i["ip"], "port": i["port"], "protocol": i["protocols"], "city": i["city"], "last_update": i["updated_at"]})
-            return result
+    
     class free_proxy_list:
         def request():
             """This function doing request to free proxy list"""
@@ -71,6 +64,7 @@ class proxyGrab:
                         result.append(result_item)
                     result_item = {}
             return result
+        
     class hidemyname:
         def request(page: int=1):
             """This function makes a request and returns a response from the resource"""
@@ -112,7 +106,48 @@ class proxyGrab:
                         result_item = {}
             return result
 
-def update_proxy_list(mode="hidemyname"):
+    class geonode():
+
+        def request():
+            request = proxy_scraper.proxyscrape.request()
+            all_data = proxy_scraper.proxyscrape.sort(request)
+            try:
+                response = requests.get(f"https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc", headers=headers)
+                response.raise_for_status()
+            except Exception as e:
+                print(f"Failed to get initial data: {e}")
+                return
+            api_data = response.json()
+            proxies = all_data
+            page = 1
+            limit = 500
+            total = api_data['total']
+            collected_data = []
+            result = []
+            while len(collected_data) < total:
+                try:
+                    response = requests.get(
+                        f"https://proxylist.geonode.com/api/proxy-list?limit={limit}&page={page}&sort_by=lastChecked&sort_type=desc",
+                        # proxies=proxies_dict,
+                        headers=headers,
+                        timeout=5
+                    )
+                    response.raise_for_status()
+                    data = response.json()
+                    collected_data.extend(data['data'])
+                    for i in data['data']:
+                        result.append({"address": i["ip"], "port": i["port"], "protocol": i["protocols"], "city": i["city"], "last_update": i["updated_at"]})
+                    if len(collected_data) >= total:
+                        break
+                    time.sleep(1)
+                    page += 1
+                except Exception as e:
+                    print(f"Proxy failed: {e}")
+                    continue
+
+            return result
+
+def update_proxy_list(mode="all"):
     """
     Mode:
         hidemyname - scrape https://hidemy.name/en/proxy-list/
@@ -120,26 +155,43 @@ def update_proxy_list(mode="hidemyname"):
         proxyscrape - scrape https://proxyscrape.com/
     """
 
-    if mode == "hidemyname":
-        output = []
-        for d in range(1, 5):
-            request = proxyGrab.hidemyname.request(page=d)
-            result = proxyGrab.hidemyname.sort(request=request)
-            for i in result:
-                output.append(i)
-        
-        return output
+    output = []
 
-    if mode == "free_proxy_list":
-        request = proxyGrab.free_proxy_list.request()
-        result = proxyGrab.free_proxy_list.sort(request)
+    if mode == "hidemyname" or mode == "all":
+        try:
+            output_arr = []
+            for d in range(1, 15):
+                request = proxy_scraper.hidemyname.request(page=d)
+                result = proxy_scraper.hidemyname.sort(request=request)
+                for i in result:
+                    output_arr.append(i)
+            output.extend(output_arr)
+        except:...
         
-        return result
+    if mode == "free_proxy_list" or mode == "all":
+        try:
+            request = proxy_scraper.free_proxy_list.request()
+            result = proxy_scraper.free_proxy_list.sort(request)
+            
+            output.extend(result)
+        except:...
 
-    if mode == "proxyscrape":
-        request = proxyGrab.proxyscrape.request(protocol="socks5")
-        result = proxyGrab.proxyscrape.sort(request)
-        return result
+    if mode == "proxyscrape" or mode == "all":
+        try:
+            request = proxy_scraper.proxyscrape.request()
+            result = proxy_scraper.proxyscrape.sort(request)
+
+            output.extend(result)
+        except:...
+
+    if mode == "geonode" or mode == "all":
+        try:
+            result = proxy_scraper.geonode.request()
+
+            output.extend(result)
+        except:...
+    
+    return output
     
 #! code section END
 
@@ -148,6 +200,7 @@ class Proxer():
         self.file_path_save = file_path_save
         self.file_path_output = file_path_output
         self.check_services = check_services
+        self.update_proxies = []
         
     def __save_proxies(self, proxy, path):
         """
@@ -196,7 +249,13 @@ class Proxer():
             nonlocal counter 
             for item in proxy_list:
                 try:
-                    proxy = item["address"] + ":" + item["port"]
+                    if item["protocol"]:
+                        if isinstance(item["protocol"], list):
+                            proxy = str(item["protocol"][0]).lower() + "://" + item["address"] + ":" + item["port"]
+                        else:
+                            proxy = str(item["protocol"]).lower() + "://" + item["address"] + ":" + item["port"]
+                    else:
+                        proxy = item["address"] + ":" + item["port"]
                     if proxy and proxy not in existing_proxies:
                         with open(path, 'a') as f:
                             f.write(proxy + "\n")
@@ -204,10 +263,12 @@ class Proxer():
                         counter += 1
                 except:
                     continue
-
+        
+        #! --- PROVIDERS ---
         add_proxies(update_proxy_list(mode="hidemyname"))
         add_proxies(update_proxy_list(mode="free_proxy_list"))
         add_proxies(update_proxy_list(mode="proxyscrape"))
+        add_proxies(update_proxy_list(mode="geonode"))
 
         if counter == 0:
             print(Fore.GREEN + "[FREE PROXY]" + Fore.WHITE + " You have the latest up-to-date proxies")
@@ -219,25 +280,32 @@ class Proxer():
         else:
             return True
 
-    def check_proxy(self, proxy, _class=False):
+    def check_proxy(self, proxy):
         """
         Checking proxy performance
         """
-        
+        pr = proxy
         proxy = proxy.replace("https://", "")
         proxy = proxy.replace("http://", "")
         proxy = proxy.strip()
         try:
-            url_index = random.randint(0, len(self.check_services)-1)
-            response = requests.get(self.check_services[url_index], proxies={"http": proxy, "https": proxy}, timeout=10)
+            url_index = random.randint(0, len(self.check_services) - 1)
+            start_time = time.time()
+            response = requests.get(self.check_services[url_index], proxies={"http": proxy, "https": proxy}, timeout=3)
+            end_time = time.time()
+            response_time = end_time - start_time
+            
             if response.status_code == 200:
-                print( Fore.GREEN + "[Working]  " + Fore.WHITE + f"{proxy}")
-                if _class == True:
-                    self.__save_proxies(proxy, self.file_path_save)
+                print(Fore.GREEN + "[Working]  " + Fore.WHITE + f"{proxy} - Response time: {response_time:.2f} seconds")
+                self.update_proxies.append(f"{pr},{response_time:.2f} sec.")
                 return True
             else:
+                print(Fore.RED + "[Not Working]  " + Fore.WHITE + f"{proxy} - Response time: {response_time:.2f} seconds")
                 return False
-        except:
+        except requests.RequestException as e:
+            end_time = time.time()
+            response_time = end_time - start_time
+            print(Fore.RED + "[Failed]  " + Fore.WHITE + f"{proxy} - Response time: {response_time:.2f} seconds - Error: {e}")
             return False
     
     def update_db_proxy(self, path="", file=False):
@@ -252,29 +320,42 @@ class Proxer():
         if path == "":
             path = self.file_path_output
 
-        if file == True:
+        if file:
             if not os.path.exists(path):
-                raise FileNotFoundError(Fore.RED + "[ERROR] There is no file at this path")
+                raise FileNotFoundError("[ERROR] There is no file at this path")
         else:
             self.parse()
 
         with open(path, "r") as file:
-            proxies = file.readlines()
+            proxies = [line.strip() for line in file.readlines()]
+        
+        start_time = time.time()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=9000) as executor:
+            future_to_proxy = {executor.submit(self.check_proxy, proxy): proxy for proxy in proxies}
+            working_proxies = []
+            for future in concurrent.futures.as_completed(future_to_proxy):
+                proxy = future_to_proxy[future]
+                try:
+                    result = future.result()
+                    if result:
+                        working_proxies.append(result)
+                except Exception as exc:
+                    print(f"Proxy {proxy} generated an exception: {exc}")
 
-        threads = []
-        for proxy in proxies:
-            thread = threading.Thread(target=self.check_proxy, args=[proxy, True])
-            thread.start()
-            threads.append(thread)
-        for thread in threads:
-            thread.join()
+        end_time = time.time()
+        print(f"Checked {len(proxies)} proxies in {end_time - start_time:.2f} seconds.")
 
+        if len(self.update_proxies) > 0:
+            for proxy in self.update_proxies:
+                with open(self.file_path_save, 'a') as f:
+                    f.write(proxy + "\n")
+        
     def get(self, count=1) -> []:
         """
         Get a free working proxy
         """
         if not os.path.exists(self.file_path_save):
-            raise FileNotFoundError(Fore.RED + "[ERROR] There is no file at this path")
+            raise FileNotFoundError(Fore.RED + "[ERROR] There is no file at this path" + Fore.RESET)
 
         total_lines = self.__count_lines(self.file_path_save)
         if total_lines == 0:
@@ -366,16 +447,16 @@ class Proxer():
                     return None
 
 if __name__ == "__main__":
-    proxy = Proxer()
+    proxy = Proxer(check_services=["https://www.google.com/"])
     
     # Parsing of new proxies, with uniqueization, if you already have this proxy, it will not add it to the file again
-    # print(proxy.parse("./test.txt"))
+    print(proxy.parse("./test.txt"))
 
     # Update the proxy list
-    # proxy.update_db_proxy()
+    proxy.update_db_proxy("./test.txt", True)
 
     # Get a working proxy
-    print(proxy.get())
+    # print(proxy.get(100))
 
     # Check proxy
     # print(proxy.check_proxy("216.80.39.89:3129"))
@@ -383,3 +464,15 @@ if __name__ == "__main__":
     # Clear DB
     # proxy.clear_db()
 
+
+# const screenProxy = new Proxy(screen, {
+#     get(target, prop) {
+#         if (prop === 'width') return 666;
+#         if (prop === 'availWidth') return 666;
+#         if (prop === 'height') return "Zalupa";
+#         if (prop === 'availHeight') return "Zalupa";
+#         return target[prop];
+#     }
+# });
+
+# window.screen = screenProxy;
